1ad82ddc691eb2ddae612c4b483522c0aaac5aa7
haeeun <leehe@swu.ac.kr>
2020-07-16 22:16:36 +0900

    Initial Commit

---
+++ src/main/java/mashup/gugugugu/GuguguguApplication.java
@@ -0,0 +1,13 @@
+package mashup.gugugugu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuguguguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuguguguApplication.class, args);
+    }
+
+}
