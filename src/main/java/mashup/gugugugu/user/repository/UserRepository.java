78d0ae6bed8f1f19b2b525f292164abc28374b0a
ihaeeun <ihaeeun16@gmail.com>
2020-09-14 22:09:59 +0900

    Add Mypage API

--- src/main/java/mashup/gugugugu/user/repository/UserRepository.java
+++ src/main/java/mashup/gugugugu/user/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package mashup.gugugugu.user.repository;
 
-public class UserRepository {
+import mashup.gugugugu.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserId(Long userId);
+    Optional<User> findByTokenId(String tokenId);
 }
