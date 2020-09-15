b78623af66039b369561029e36a981acf9d10e42
ihaeeun <ihaeeun16@gmail.com>
2020-09-15 11:24:49 +0900

    Add Sign in, Sign up API

--- src/main/java/mashup/gugugugu/user/entity/User.java
+++ src/main/java/mashup/gugugugu/user/entity/User.java
@@ -1,4 +1,42 @@
 package mashup.gugugugu.user.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mashup.gugugugu.user.dto.SignInRequestDto;
+import mashup.gugugugu.user.dto.SignUpRequestDto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
+    @Id
+    private Long userId;
+    private String tokenId;
+    private String name;
+    private String nickname;
+    private String email;
+    private Boolean subscribe;
+
+    private User(SignUpRequestDto requestBody) {
+        this.tokenId = requestBody.getTokenId();
+        this.name = requestBody.getName();
+        this.nickname = requestBody.getNickname();
+        this.email = requestBody.getEmail();
+        this.subscribe = requestBody.getSubscribe();
+    }
+
+    public static User from(SignUpRequestDto requestBody){
+        return new User(requestBody);
+    }
+
+    public void setSubscribe(Boolean subscribe){
+        this.subscribe = subscribe;
+    }
+
+    public void updateNickname(String nickname){
+        this.nickname = nickname;
+    }
 }
