4d4377da2d7c463f8967197ead7567d4f2248ae8
ihaeeun <ihaeeun16@gmail.com>
2020-09-23 19:44:55 +0900

    Add JwtService

--- src/main/java/mashup/gugugugu/user/controller/UserController.java
+++ src/main/java/mashup/gugugugu/user/controller/UserController.java
@@ -2,12 +2,11 @@ package mashup.gugugugu.user.controller;
 
 import mashup.gugugugu.user.dto.SignInRequestDto;
 import mashup.gugugugu.user.dto.SignUpRequestDto;
+import mashup.gugugugu.user.service.JwtService;
 import mashup.gugugugu.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService) {
         this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/sign-up")
@@ -31,8 +32,9 @@ public class UserController {
     @PostMapping("/sign-in")
     public ResponseEntity signIn(@RequestBody SignInRequestDto requestBody){
         if(userService.isExisted(requestBody.getTokenId(), requestBody.getEmail())){
-            // TODO : jws 생성후 header에 넣어줘야 함
+            String jws = jwtService.create(requestBody);
             HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", jws);
 
             // TODO : 편지함으로 이동
             return ResponseEntity.status(HttpStatus.OK).headers(headers).build();
