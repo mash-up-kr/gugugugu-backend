849b19a386c40e289c152b0180a90dcffff8687e
Haeeun Lee <44107947+ihaeeun@users.noreply.github.com>
2020-09-23 19:16:34 +0900

    Feature/2 log in (#5)
    * Add Sign in, Sign up API
    
    * Modify sign-up
    
    * Add SignUp Service Test

--- src/main/java/mashup/gugugugu/user/controller/UserController.java
+++ src/main/java/mashup/gugugugu/user/controller/UserController.java
@@ -1,4 +1,43 @@
 package mashup.gugugugu.user.controller;
 
+import mashup.gugugugu.user.dto.SignInRequestDto;
+import mashup.gugugugu.user.dto.SignUpRequestDto;
+import mashup.gugugugu.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity signUp(@RequestBody SignUpRequestDto requestBody){
+        userService.signUp(requestBody);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity signIn(@RequestBody SignInRequestDto requestBody){
+        if(userService.isExisted(requestBody.getTokenId(), requestBody.getEmail())){
+            // TODO : jws 생성후 header에 넣어줘야 함
+            HttpHeaders headers = new HttpHeaders();
+
+            // TODO : 편지함으로 이동
+            return ResponseEntity.status(HttpStatus.OK).headers(headers).build();
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
--- src/main/java/mashup/gugugugu/user/service/UserService.java
+++ src/main/java/mashup/gugugugu/user/service/UserService.java
@@ -1,4 +1,34 @@
 package mashup.gugugugu.user.service;
 
+import mashup.gugugugu.exception.BaseException;
+import mashup.gugugugu.exception.ErrorModel;
+import mashup.gugugugu.user.dto.SignUpRequestDto;
+import mashup.gugugugu.user.entity.User;
+import mashup.gugugugu.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean isExisted(String tokenId, String email){
+        return userRepository.findByTokenId(tokenId).filter(user -> user.getEmail().equals(email)).isPresent();
+    }
+
+    @Transactional
+    public void signUp(SignUpRequestDto requestBody){
+        if (isExisted(requestBody.getTokenId(), requestBody.getEmail())){
+            throw new BaseException(ErrorModel.builder().build());
+        }
+
+        User user = User.from(requestBody);
+
+        userRepository.save(user);
+    }
+
 }
